776ca2b742d36c347e844b49be58c5b45504cc40
Colin Narans <cnarans@gmail.com>
2016-07-25 14:25:17 -0500

    basic converter functions

--- app/models/converter.rb
+++ app/models/converter.rb
@@ -1,43 +1,45 @@
 class Converter
 
 	def kmToMiles(km)
-
+		return km/1.60934
 	end
 
 	def milesToKm(miles)
-
+		return miles*1.60934
 	end
 
 	def celsiusToFahrenheit(celsius)
-
+		return celsius*1.8+32
 	end
 
 	def fahrenheitToCelsius(fahrenheit)
-
+		return (fahrenheit-32)/1.8
 	end
 
 	def stonesToPounds(stones)
-
+		return stones*14
 	end
 
 	def poundsToStones(pounds)
-
+		return pounds/14
 	end
 
 	def poundsToKg(pounds)
-
+		return pounds/2.20462
 	end
 
 	def kgToPounds(kg)
-
+		return kg*2.20462
 	end
 
 	def stonesToKg(stones)
-
+		return stones*6.35029
 	end
 
 	def kgToStones(kg)
-
+		return kg/6.35029
 	end
-
 end
+
+test = Converter.new
+puts test.milesToKm(1)
