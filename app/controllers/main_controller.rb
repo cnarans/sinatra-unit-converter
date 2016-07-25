7a66219b74e6b413af2d811f8fa848629a47c247
Colin Narans <cnarans@gmail.com>
2016-07-25 15:34:56 -0500

    basic form and results page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,13 @@
 MyApp.get "/" do
-  erb :"main"
+  	erb :"main"
+end
+
+MyApp.post "/milesToKm" do
+	@option = params[:option]
+	@units = params[:units].to_f
+	@initial = "Miles"
+	@target = "km"
+	con = Converter.new
+	@convert = con.milesToKm(@units)
+	erb :"result"
 end
