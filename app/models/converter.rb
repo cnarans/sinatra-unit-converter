187772221c999c9ef1bc6e723b4103da59d9bb5a
Colin Narans <cnarans@gmail.com>
2016-07-25 14:12:13 -0500

    class outline

---
+++ app/models/converter.rb
@@ -0,0 +1,43 @@
+class Converter
+
+	def kmToMiles(km)
+
+	end
+
+	def milesToKm(miles)
+
+	end
+
+	def celsiusToFahrenheit(celsius)
+
+	end
+
+	def fahrenheitToCelsius(fahrenheit)
+
+	end
+
+	def stonesToPounds(stones)
+
+	end
+
+	def poundsToStones(pounds)
+
+	end
+
+	def poundsToKg(pounds)
+
+	end
+
+	def kgToPounds(kg)
+
+	end
+
+	def stonesToKg(stones)
+
+	end
+
+	def kgToStones(kg)
+
+	end
+
+end
